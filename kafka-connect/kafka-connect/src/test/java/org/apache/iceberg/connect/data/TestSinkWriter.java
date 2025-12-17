f7fd17bc90d4a17cdafccb0a74be2ff4fefddd87
Devendra Parhate <dparhate@newrelic.com>
2025-12-17 13:52:52 +0530

    Add tests

--- kafka-connect/kafka-connect/src/test/java/org/apache/iceberg/connect/data/TestSinkWriter.java
+++ kafka-connect/kafka-connect/src/test/java/org/apache/iceberg/connect/data/TestSinkWriter.java
@@ -172,7 +172,7 @@ public class TestSinkWriter {
   }
 
   private List<IcebergWriterResult> sinkWriterTest(
-          Map<String, Object> value, IcebergSinkConfig config) {
+      Map<String, Object> value, IcebergSinkConfig config) {
     return sinkWriterTest(value, config, null);
   }
 
@@ -225,7 +225,6 @@ public class TestSinkWriter {
     when(config.tableConfig(any())).thenReturn(mock(TableSinkConfig.class));
     when(config.errorTolerance()).thenReturn(ErrorTolerance.ALL.toString());
     when(config.errorLogIncludeMessages()).thenReturn(true);
-
 
     Map<String, Object> value = ImmutableMap.of("id", 1);
     List<IcebergWriterResult> writerResults = sinkWriterTest(value, config);
