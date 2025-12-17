9a5a98bced6688cace2ea7e8b061a8993f6c3274
Devendra Parhate <dparhate@newrelic.com>
2025-12-17 12:47:22 +0530

    Update DLQ implementation to use errant reporter

--- kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/channel/CommitterImpl.java
+++ kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/channel/CommitterImpl.java
@@ -178,6 +178,7 @@ public class CommitterImpl implements Committer {
     if (null == this.worker) {
       LOG.info("Starting commit worker");
       SinkWriter sinkWriter = new SinkWriter(catalog, config);
+      sinkWriter.setReporter(context.errantRecordReporter());
       worker = new Worker(config, clientFactory, sinkWriter, context);
       worker.start();
     }
